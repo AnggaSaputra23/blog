94dd13e12803f151d21c142770ba14f9a342e1e3
AnggaSaputra23 <autoruns18@gmail.com>
2022-11-24 08:56:30 +0800

    update kamis

---
+++ database/migrations/2022_11_22_010803_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Konten', function (Blueprint $table) {
+            $table->id();
+            $table->string('Judul')->nulable();
+            $table->text('Konten')->nullble();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Konten');
+    }
+}
